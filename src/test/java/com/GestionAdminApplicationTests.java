38165d97a5aa6565a79f24712ffff60448a52bb5
Pauline-Humbert <42661326+Pauline-Humbert@users.noreply.github.com>
2020-11-18 16:18:07 +0100

    initialisation gestion admin

---
+++ src/test/java/com/GestionAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
