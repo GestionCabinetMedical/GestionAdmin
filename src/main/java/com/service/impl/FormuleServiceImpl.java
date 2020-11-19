9d1bfb93545087465db6109e1efa12fab8fa537e
Maxime Rembert <maxime.rembert@gmail.com>
2020-11-19 10:11:17 +0100

    Ajout champ specialite

--- src/main/java/com/service/impl/FormuleServiceImpl.java
+++ src/main/java/com/service/impl/FormuleServiceImpl.java
@@ -3,12 +3,16 @@
  */
 package com.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.entity.Formule;
-import com.repo.IDaoRepo;
+import com.repo.IFormuleRepo;
 import com.service.IFormuleService;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Classe service de formule implémentant IFormuleService.
@@ -17,9 +21,23 @@ import com.service.IFormuleService;
  *
  */
 @Service
-public class FormuleServiceImpl {
+@Slf4j
+public class FormuleServiceImpl extends DaoServiceImpl<Formule> implements IFormuleService {
 
 	@Autowired
-	private IDaoRepo<Formule> repo;
+	private IFormuleRepo repo;
+
+	@Override
+	public Formule findBySpecialite(String specialite) {
+		log.info("Classe Formule service : Méthode findBySpecialite appelée");
+		if (specialite != null) {
+			log.info("Appel repo OK");
+			return repo.findBySpecialite(specialite);
+		}
+		log.warn("Erreur methode find By Specialite : specialite null !");
+		return null;
+	}
+
+
 
 }
